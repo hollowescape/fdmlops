54808e7ace46d4ee89cf1e43adfe361f77b5914b
hollowescape <sharma.mayankguru@gmail.com>
2022-06-18 16:46:45 +0530

    files

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mayank/Documents/final/test/django-crud-ajax-login-register-fileupload/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
